201139b1bb710e05554da6439efa323b43ddc993
Diego Rodriguez Casillas <drodcasi@emeal.nttdata.com>
2023-03-14 19:27:57 +0100

    added Viper

---
+++ Formula1/ViperModel/ViewModel/XViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  XViewModel.swift
+//  MiProyecto
+//
+//  Created by Diego Rodriguez Casillas on 8/3/23.
+//
+
+import Foundation
+import VIPERPLUS
+import UIKit
+
+class XViewModel: BaseViewModel {
+
+}
